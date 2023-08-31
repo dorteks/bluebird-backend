71498ebfa21f3ccc7322c8cc61b9da7889ddae9b
dorteks <ayodelealayemi@gmail.com>
2023-08-31 09:40:04 +0100

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import cors from "cors";
 import express from "express";
 import jwt from "jsonwebtoken";
 import { config } from "./configs";
@@ -9,6 +10,7 @@ import { authenticate, verifyToken } from "./middleware/auth/jwtAuth";
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(cookieParser("", {}));
 
